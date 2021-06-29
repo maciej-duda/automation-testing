db36d77cbd60048d6ac94b42914bbf8d4094f2cd
maciej.duda1 <maciej.duda@testarmy.com>
2021-06-29 12:28:43 +0200

    basic API login

--- cypress/integration/adminTests/courseCreation.js
+++ cypress/integration/adminTests/courseCreation.js
@@ -3,10 +3,14 @@ import LoginPage from '../../pageObjects/courseCreation/loginPage';
 const loginPage = new LoginPage();
 
 describe('Course creation by the teacher', () => {
+   beforeEach(() => {
+        cy.loginAsAdminUsingApi();
+      });
 
 	it('Login as a teacher', () => {
         loginPage.visitTestPage();
-		loginPage.checkIfBasicElementsAreDisplayed();
-        loginPage.provideLoginCredentials(Cypress.env('testUserEmail'), '1qazXSW@')
+		//loginPage.checkIfBasicElementsAreDisplayed();
+        //loginPage.provideLoginCredentials('smoke_teacher01@schoolified.com', '1qazXSW@');
+        
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,28 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginUsingApi", (username, password) => {
+    cy.request({
+      method: "POST",
+      url: "https://uat.api.schoolified.com/api/auth/login",
+     
+
+      body: {
+        username: username,
+        password: password
+      },
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+      cy.log("0." + JSON.stringify(resp.body));
+      
+      cy.log("1. " + resp.body.result.token);
+      window.localStorage.setItem("token", resp.body.result.token);
+    });
+  });
+
+  Cypress.Commands.add("loginAsAdminUsingApi", () => {
+    cy.fixture(`teacherCredentials.json`).then((teacherCredentials) => {
+      cy.loginUsingApi(Cypress.env('testUserEmail'), teacherCredentials.password);
+    });
+  });
